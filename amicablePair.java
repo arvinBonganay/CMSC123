e77e38ea149e267ea3e64185155a909530f5fd1f
arvinBonganay <arvinb450@gmail.com>
2016-09-29 00:52:40 +0800

    Update amicablePair.java

--- amicablePair.java
+++ amicablePair.java
@@ -2,21 +2,33 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class GFG {
+/*
+*	September 29, 2016
+*
+* 	Amicable numbers are two different numbers whose sum of factors of one number excluding itself is equal to the other number.
+*
+*	Input : First line contains the number of testcases. Next n lines contains 2 space-separated integers.
+*	Output : Sum of the factors of each number and then determines whether the two numbers are amicable pair.
+*		
+*/
+
+class amicablePair {
+	// sum of the factors a number x excluding the number itself
     public static int factorSum(int x){
         int sum = 1;
         for(int i = 2; i <= Math.sqrt(x); i++){
             if (x % i == 0){
                 if (x / i == i){
-                    sum+=i;
+                    sum += i;
                 } else {
-                    sum+=i+x/i;
+                    sum += i + x/i;
                 }
             }
         }
         return sum;
     }
     
+	// determines if two numbers x and y are an amicable pair
     public static boolean amicablePair(int x, int y){
         if (factorSum(x) == y && factorSum(y) == x){
             return true;
@@ -25,16 +37,16 @@ class GFG {
         }
     }
     
-	public static void main (String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int x, y;
-		for (int i = 0; i < n; i++){
-		    x = sc.nextInt();
-		    y = sc.nextInt();
-		    System.out.println(factorSum(x));
-		    System.out.println(factorSum(y));
-		    System.out.println(amicablePair(x, y));
-		}
-	}
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x, y;
+        for (int i = 0; i < n; i++){
+            x = sc.nextInt();
+            y = sc.nextInt();
+            System.out.printf("Sum of the factors of %d: %d\n", x, factorSum(x));
+            System.out.printf("Sum of the factors of %d: %d\n", y, factorSum(y));
+            System.out.printf("%d and %d are amicable pairs: %b\n", x, y, amicablePair(x, y));
+        }
+    }
 }
